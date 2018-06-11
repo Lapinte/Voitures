c239bf0ad4c54cec7e627c20adcca0b0b5803d14
fatimata <fatimata.mbaye@hotmail.com>
2018-06-11 10:39:17 +0200

    voiture

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Voitures
     {
         static void Main(string[] args)
         {
+            //Création de la connexion
+            var connectionString = @"Server=EMER09\SQLEXPRESS;Database=Voitures;Trusted_Connection=True";
+
+            var connexion = new SqlConnection(connectionString);
+
+            //Création d'une commande
+            var commande = new SqlCommand("SELECT * FROM Marques", connexion);
+
+            // Mode connecté
+            connexion.Open();
+            SqlDataReader dataReader = commande.ExecuteReader();
+            while(dataReader.Read())
+            {
+                Console.WriteLine($"Id:{dataReader.GetInt32(0)}, Nom:{dataReader.GetString(1)}");
+            }
+
+            Console.ReadKey();
+
+            connexion.Close();
         }
     }
 }
